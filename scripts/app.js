e04e33544c3dd7c35e04906ad99d3555ef7ba895
marijaaleksovska <55788958+marijaaleksovska@users.noreply.github.com>
2020-03-25 17:35:38 +0100

    Add files via upload

---
+++ scripts/app.js
@@ -0,0 +1,119 @@
+axios.get('https://covid-ca.azurewebsites.net/api/covid/countries',{
+    headers:{
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+})
+.then((response)=>{
+    let data =JSON.parse(response.data);
+    console.log(data);
+    let tb=document.getElementById("tbody");
+  
+    for(let i=0;i<data.length;i++){
+        let tr=document.createElement("tr");
+        let td1=document.createElement("td");
+        td1.innerHTML=data[i].country;
+        td1.style.fontWeight="bold";
+        tr.appendChild(td1);
+
+        let td2=document.createElement("td");
+        td2.innerHTML=data[i].cases;
+        tr.appendChild(td2);
+
+        let td3=document.createElement("td");
+        td3.innerHTML="+"+data[i].todayCases;
+        tr.appendChild(td3);
+
+        let td4=document.createElement("td");
+        td4.innerHTML=data[i].deaths;
+        tr.appendChild(td4);
+
+        let td5=document.createElement("td");
+        td5.innerHTML="+"+data[i].todayDeaths;
+        tr.appendChild(td5);
+
+        let td6=document.createElement("td");
+        td6.innerHTML=data[i].recovered;
+        tr.appendChild(td6);
+
+        let td8=document.createElement("td");
+        td8.innerHTML=data[i].critical;
+        tr.appendChild(td8);
+        
+        tb.appendChild(tr);
+
+
+  
+    }
+    
+}).catch(error=>{
+    console.log(error)
+});
+
+axios.get('https://covid-ca.azurewebsites.net/api/covid/overview',{
+    headers:{
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+})
+.then((response)=>{
+    let data =JSON.parse(response.data);
+    console.log(data);
+    let ct1=document.getElementById("ct-1");
+    ct1.innerHTML=data.cases;
+    let ct2=document.getElementById("ct-2");
+    ct2.innerHTML=data.deaths;
+    let ct3=document.getElementById("ct-3");
+    ct3.innerHTML=data.recovered;
+}).catch(error=>{
+    console.log(error)
+});
+
+axios.get('https://covid-ca.azurewebsites.net/api/covid/countries',{
+    headers:{
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+})
+.then((response)=>{
+    let data =JSON.parse(response.data);
+    console.log(data);
+    google.charts.load('current', {
+        'packages':['geochart'],'mapsApiKey': 'AIzaSyCZ6arsk9pY6-cweBJ8VkjAoDsbC-0Qqvg'
+      });
+      google.charts.setOnLoadCallback(drawRegionsMap);
+     function drawRegionsMap(){
+        var data1 = new google.visualization.DataTable();
+        data1.addColumn('string', 'Земја');
+        data1.addColumn('number', 'Вкупно случаи');
+       
+        for(let i=0;i<data.length;i++){
+            if(data[i].country=="USA"){
+                data1.addRow(['United States',data[i].cases]);
+            }
+            if(data[i].country=="Czechia"){
+                data1.addRow(['Czech Republic',data[i].cases]);
+            }
+            if(data[i].country=="North Macedonia"){
+                data1.addRow(['Macedonia',data[i].cases]);
+            }
+            data1.addRow([data[i].country,data[i].cases])
+        }
+        var options = {
+           
+            colorAxis: {colors: ['#C5E8B7','#57C84D','#2EB62C','#00853f','yellow', 'orange', '#e31b23']},
+        
+            datalessRegionColor: '#C5E8B7',
+            defaultColor: '#C5E8B7',
+          };
+
+    var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+
+    chart.draw(data1, options);
+     }
+    
+    
+    
+}).catch(error=>{
+    console.log(error)
+});
---
+++ scripts/app1.js
@@ -0,0 +1,76 @@
+axios.get('https://covid-ca.azurewebsites.net/api/covid/countries',{
+    headers:{
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+})
+.then((response)=>{
+    let data =JSON.parse(response.data);
+    console.log(data);
+    let tb=document.getElementById("tbody");
+    for(let i=0;i<data.length;i++){
+        if(data[i].country=="North Macedonia"){
+            var mkd=data[i];
+            console.log(mkd);
+
+            let ct1=document.getElementById("ct-4");
+            ct1.innerHTML=mkd.cases;
+            
+            let ct2=document.getElementById("ct-5");
+            ct2.innerHTML=mkd.deaths;
+
+            let ct3=document.getElementById("ct-6");
+            ct3.innerHTML=mkd.recovered;
+
+            let ct7=document.getElementById("ct-7");
+            ct7.innerHTML="+"+mkd.todayCases;
+        }
+    }
+}).catch(error=>{
+    console.log(error);
+});
+
+axios.get('https://covid-ca.azurewebsites.net/api/covid/history',{
+    headers:{
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+    }
+})
+.then((response)=>{
+    let data =JSON.parse(response.data);
+    console.log(data);
+    let mkdData;
+    for(let i=0;i<data.length;i++){
+        if(data[i].country=="north macedonia"){
+            mkdData=data[i];
+        }
+    }
+    console.log(mkdData);
+    console.log(Object.keys(mkdData.timeline.cases));
+    var date=Object.keys(mkdData.timeline.cases);
+    var values=Object.values(mkdData.timeline.cases);
+    console.log(date);
+    console.log(values);
+    var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: date,
+        datasets: [{
+            label: 'Вкупно случаи',
+            borderColor: 'darkgreen',
+            data: values
+        }]
+    },
+
+    options: {}
+});
+  
+
+   
+}).catch(error=>{
+    console.log(error);
+});
